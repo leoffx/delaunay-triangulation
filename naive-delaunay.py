d2902f3e33e008cab8a5364fdcc8b98e233ea7a9
leoffx <43040272+leoffx@users.noreply.github.com>
2018-11-21 07:59:32 -0200

    Add files via upload
    added ien

--- naive-delaunay.py
+++ naive-delaunay.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
-import matplotlib.tri as tri
 import numpy as np
 
-point_num = 100  #número de pontos aleatórios
+point_num = 50  #número de pontos aleatórios
 
 points = np.random.rand(point_num, 2)  #inicializa pontos aleatórios
+ien = []
+fig, ax = plt.subplots()
 
 
 def CCW(
@@ -15,7 +16,7 @@ def CCW(
 
 def delaunay(
         a, b, c, d
-):  #determina se o quarto ponto está dentro da circunferencia // precisa que a,b,c estejam em ordem anti horaria
+):  #determina se o quarto ponto está dentro da circunferencia // precisa que a,b,c esteja em ordem anti horaria
     det = np.linalg.det(
         [[a[0] - d[0], a[1] - d[1], (a[0] - d[0])**2 + (a[1] - d[1])**2],
          [b[0] - d[0], b[1] - d[1], (b[0] - d[0])**2 + (b[1] - d[1])**2],
@@ -44,6 +45,12 @@ for i in range(point_num):  #fixa primeiro ponto da triangulaçao
                     plt.plot([p[0], q[0]], [p[1], q[1]], 'r-', lineWidth=.5)
                     plt.plot([r[0], q[0]], [r[1], q[1]], 'r-', lineWidth=.5)
                     plt.plot([p[0], r[0]], [p[1], r[1]], 'r-', lineWidth=.5)
+                    ien = np.append(ien, [i, j, k])
 
+for i, txt in enumerate(range(point_num)):
+    ax.annotate(txt, (points[i, 0], points[i, 1]))
+
+ien = np.reshape(ien, (-1, 3))
+print(ien.shape, ien)
 plt.plot(points[:, 0], points[:, 1], '.')
 plt.show()
